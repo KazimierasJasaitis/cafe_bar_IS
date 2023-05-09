969fff68e017ae978c2a49bc8272380bfad25a95
kaja7807 <kazimieras.jasaitis@gmail.com>
2023-05-09 23:48:02 +0300

    Added new tables, resources, separated request methods, added content headers

--- README.md
+++ README.md
@@ -1,10 +1,40 @@
 # Cafe and Bar Information System
 
-This is a web service that manages information about cafes and bars.
+## How to run
 
-# API Endpoints
-GET /cafes_and_bars - Get all cafes and bars.
-GET /cafes_and_bars/<id> - Get cafe or bar by ID.
-POST /cafes_and_bars - Create a new cafe or bar.
-PUT /cafes_and_bars/<id> - Update a cafe or bar by ID.
-DELETE /cafes_and_bars/<id> - Delete a cafe or bar by ID.
+git clone https://github.com/KazimierasJasaitis/cafe_bar_IS.git
+cd cafe_bar_IS
+make
+
+## API Endpoints
+### Users
+
+- `POST /user`: Create a new user.
+- `GET /user`: Get a list of all users.
+- `GET /user/<username>`: Get a specific user by username.
+- `PUT /user/<username>`: Update a user by username.
+- `DELETE /user/<username>`: Delete a user by username.
+
+### BarCafe
+
+- `POST /bar_cafe`: Create a new bar/cafe.
+- `GET /bar_cafe`: Get a list of all bars/cafes.
+- `GET /bar_cafe/<id>`: Get a specific bar/cafe by ID.
+- `PUT /bar_cafe/<id>`: Update a bar/cafe by ID.
+- `DELETE /bar_cafe/<id>`: Delete a bar/cafe by ID.
+
+### SeatStatus
+
+- `POST /seat_status`: Create a new seat status.
+- `GET /seat_status`: Get a list of all seat statuses.
+- `GET /seat_status/<id>`: Get a specific seat status by ID.
+- `PUT /seat_status/<id>`: Update a seat status by ID.
+- `DELETE /seat_status/<id>`: Delete a seat status by ID.
+
+### KarmaLog
+
+- `POST /karma_log`: Create a new karma log.
+- `GET /karma_log`: Get a list of all karma logs.
+- `GET /karma_log/<id>`: Get a specific karma log by ID.
+- `PUT /karma_log/<id>`: Update a karma log by ID.
+- `DELETE /karma_log/<id>`: Delete a karma log by ID.
--- data.py
+++ data.py
@@ -1,12 +1,37 @@
-def populate_initial_data(session, CafeBar):
-    if session.query(CafeBar).count() == 0:
-        initial_data = [
+def populate_initial_data(session, User, BarCafe, SeatStatus, KarmaLog):
+    if session.query(User).count() == 0:
+        initial_users = [
+            {
+                "username": "jonas1998",
+                "password": "password123",
+                "email": "jonaszirnys@gmail.com",
+                "role": "admin",
+                "managed_cafe_id": None,
+                "reputation_index": 100,
+            },
+            {
+                "username": "xxxjurgitaxxx",
+                "password": "katinas123",
+                "email": "jurgita@yahoo.com",
+                "role": "user",
+                "managed_cafe_id": None,
+                "reputation_index": 5,
+            },
+        ]
+
+        for data in initial_users:
+            user = User(**data)
+            session.add(user)
+
+    if session.query(BarCafe).count() == 0:
+        initial_bar_cafes = [
             {
                 "name": "Coffee spells",
                 "location": "Pylimo g. 38C",
                 "open_hours": "08:00-17:00",
                 "type": "cafe",
                 "seats": 18,
+                "user_id": "jonas1998",
             },
             {
                 "name": "Local pub",
@@ -14,6 +39,7 @@ def populate_initial_data(session, CafeBar):
                 "open_hours": "16:00-00:00",
                 "type": "bar",
                 "seats": 25,
+                "user_id": "jonas1998",
             },
             {
                 "name": "Lola",
@@ -21,11 +47,50 @@ def populate_initial_data(session, CafeBar):
                 "open_hours": "8:00-18:00",
                 "type": "cafe",
                 "seats": 40,
+                "user_id": "jonas1998",
             },
         ]
 
-        for data in initial_data:
-            cafe_bar = CafeBar(**data)
-            session.add(cafe_bar)
+        for data in initial_bar_cafes:
+            bar_cafe = BarCafe(**data)
+            session.add(bar_cafe)
 
-        session.commit()
+    if session.query(SeatStatus).count() == 0:
+        initial_seat_statuses = [
+            {
+                "bar_cafe_id": 1,
+                "user_id": "jonas1998",
+                "status": "occupied",
+                "timestamp": "2023-05-01T10:00:00",
+            },
+            {
+                "bar_cafe_id": 2,
+                "user_id": "xxxjurgitaxxx",
+                "status": "empty",
+                "timestamp": "2023-05-01T11:00:00",
+            },
+        ]
+
+        for data in initial_seat_statuses:
+            seat_status = SeatStatus(**data)
+            session.add(seat_status)
+
+    if session.query(KarmaLog).count() == 0:
+        initial_karma_logs = [
+            {
+                "user_id": "jonas1998",
+                "karma_points": 5,
+                "timestamp": "2023-05-01T12:00:00",
+            },
+            {
+                "user_id": "xxxjurgitaxxx",
+                "karma_points": -3,
+                "timestamp": "2023-05-01T13:00:00",
+            },
+        ]
+
+        for data in initial_karma_logs:
+            karma_log = KarmaLog(**data)
+            session.add(karma_log)
+
+    session.commit()
